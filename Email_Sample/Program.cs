a63e946d6af9b42d601100199cbf06fe3c7dd0ec
Harshita <chandaliyaharshita222@gmail.com>
2023-05-17 20:18:27 +0530

    Added Welcome Message

---
+++ Email_Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Email_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Email Smaple Program using REGEX and Pattern!");
+        }
+    }
+}
