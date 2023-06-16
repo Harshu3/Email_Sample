9b14e47691da4ed2f0b8662a1372c6efa795c029
Harshita <chandaliyaharshita222@gmail.com>
2023-06-16 19:00:53 +0530

    Email Samples

--- Email_Sample/Program.cs
+++ Email_Sample/Program.cs
@@ -7,6 +7,30 @@ namespace Email_Sample
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Email Smaple Program using REGEX and Pattern!");
+            ValidEmail valid = new ValidEmail();
+            Console.WriteLine("Valid Email Samples are:");
+            Console.WriteLine(valid.CheckValidEmail("abc@yahoo.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc-100@yahoo.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc.100@yahoo.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc111@abc.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc-100@abc.net"));
+            Console.WriteLine(valid.CheckValidEmail("abc.100@abc.com.au"));
+            Console.WriteLine(valid.CheckValidEmail("abc@1.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc@gmail.com.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc+100@gmail.com"));
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Invalid Email Samples are:");
+            Console.WriteLine(valid.CheckValidEmail("abc"));
+            Console.WriteLine(valid.CheckValidEmail("abc@.com.my"));
+            Console.WriteLine(valid.CheckValidEmail("abc123@gmail.a"));
+            Console.WriteLine(valid.CheckValidEmail("abc123@.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc123@.com.com"));
+            Console.WriteLine(valid.CheckValidEmail(".abc@abc.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc()*@gmail.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc@%*.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc..2002@gmail.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc.@gmail.com"));
+            Console.WriteLine(valid.CheckValidEmail("abc@abc@gmail.com"));
         }
     }
 }
---
+++ Email_Sample/ValidEmail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_Sample
+{
+    public class ValidEmail
+    {
+        public string email = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.com|net(.com|au)?$";
+
+        public string CheckValidEmail(string data)
+        {
+            Console.Write("\n" + data + ": ");
+            string res = Regex.IsMatch(data, email) ? "Valid" : "Invalid";
+            return res;
+        }
+    }
+}
